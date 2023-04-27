28a4d90f6ec1991c40a6d397b70de83441373251
Rakshit2622 <rakshit.sakhuja345@gmail.com>
2023-04-27 21:40:42 +0530

    spelling of registration corrected

--- users/urls.py
+++ users/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from users import views as users_views
 
 urlpatterns=[
-	path('register/vendor-registeration/',users_views.vendor_register,name='vendor-register-view'),
-	path('register/customer-registeration/',users_views.customer_register,name='customer-register-view'),
+	path('register/vendor-registration/',users_views.vendor_register,name='vendor-register-view'),
+	path('register/customer-registration/',users_views.customer_register,name='customer-register-view'),
 	path('register/',users_views.register_view,name='register'),
 	path('vendor-profile/',users_views.vendor_profile,name='vendor-profile'),
 ]
